a9519f137ab3ede97fbcca3c3e7ba7f7ad73d2f0
Aryan Shah <aryanshah791@gmail.com>
2021-02-19 22:45:15 -0500

    Add status file

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ export PORT=4791
 
 echo "Stopping old copy of app, if any..."
 
-_build/prod/rel/practice/bin/practice stop || true
+_build/prod/rel/bulls/bin/bulls stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/practice/bin/practice start
+_build/prod/rel/bulls/bin/bulls start
 
